2b2c9bdfef398e3d6a07d382a6b7acfbe766abf9
numericdao <103108066+numericdao@users.noreply.github.com>
2022-04-28 22:19:59 +0530

    sale timer

---
+++ sale/timer.js
@@ -0,0 +1,23 @@
+function updateTimer() {
+    future = Date.parse("april 29, 2022 10:30:00 am utc");
+    now = new Date();
+    diff = future - now;
+
+    days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    hours = Math.floor(diff / (1000 * 60 * 60));
+    mins = Math.floor(diff / (1000 * 60));
+    secs = Math.floor(diff / 1000);
+
+    d = days;
+    h = hours - days * 24;
+    m = mins - hours * 60;
+    s = secs - mins * 60;
+
+    document.getElementById("timer")
+        .innerHTML =
+        '<div><span>' + d + '</span> <span class="stat_title"> days </span>' +
+        '<span>' + h + '</span><span class="stat_title"> hours </span>' +
+        '<span>' + m + '</span><span class="stat_title"> minutes </span>' +
+        '<span>' + s + '</span><span class="stat_title"> seconds </span></div>';
+}
+setInterval('updateTimer()', 1000);
